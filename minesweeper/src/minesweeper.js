830e44fa3bea58e411d3ed51e86fbebb17062efe
DamianPraca <watek-177@o2.pl>
2018-01-17 09:30:07 +0100

    zad 7 8 9 cz2 sapera

--- minesweeper/src/minesweeper.js
+++ minesweeper/src/minesweeper.js
@@ -1,8 +1,9 @@
+
 const blankLine = '  |   |  ';
 const guessLine = '1 |   |  ';
 const bombLine = '  | B |  ';
 
-console.log("This is what an empty board would look like");
+/*console.log("This is what an empty board would look like");
 console.log(blankLine);
 console.log(blankLine);
 console.log(blankLine);
@@ -11,4 +12,23 @@ console.log(blankLine);
 console.log("This is what an empty board would look like");
 console.log(guessLine);
 console.log(bombLine);
-console.log(blankLine);
+console.log(blankLine);*/
+
+const board = [
+	[' ', ' ', ' '], 
+	[' ', ' ', ' '], 
+	[' ', ' ', ' ']
+];
+
+const printBoard = board => {
+	console.log('Current Board: ');
+	console.log(board[0].join(' | '));
+	console.log(board[1].join(' | '));
+	console.log(board[2].join(' | '));
+};
+printBoard(board);
+
+board[0][1] = '1';
+board[2][2] = 'B';
+
+printBoard(board);
