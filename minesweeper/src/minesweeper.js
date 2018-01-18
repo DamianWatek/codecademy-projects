353f8e8e5bb82903361768eee7ca140e84d95365
DamianPraca <watek-177@o2.pl>
2018-01-18 10:45:42 +0100

    saper part 3

--- minesweeper/src/minesweeper.js
+++ minesweeper/src/minesweeper.js
@@ -1,34 +1,93 @@
 
-const blankLine = '  |   |  ';
-const guessLine = '1 |   |  ';
-const bombLine = '  | B |  ';
+// PART II 
 
-/*console.log("This is what an empty board would look like");
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-
-
-console.log("This is what an empty board would look like");
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);*/
-
-const board = [
+/*const board = [
 	[' ', ' ', ' '], 
 	[' ', ' ', ' '], 
 	[' ', ' ', ' ']
-];
+];*/
 
-const printBoard = board => {
-	console.log('Current Board: ');
-	console.log(board[0].join(' | '));
-	console.log(board[1].join(' | '));
-	console.log(board[2].join(' | '));
-};
-printBoard(board);
+
+/*printBoard(board);
 
 board[0][1] = '1';
 board[2][2] = 'B';
 
-printBoard(board);
+printBoard(board);*/
+
+
+/* PART III */
+
+// Fukncja generowania planszy do gry
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+	
+	let board = [];
+	
+	for (let indexRows = 0; indexRows < numberOfRows; indexRows++) {
+		let row = [];
+		for (let indexColumn = 0;  indexColumn < numberOfColumns; indexColumn++) {
+			row.push(' ');
+		}
+		board.push(row);
+	}
+	
+	return board;
+};
+
+// Generowanie bomb 
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+	let board = [];
+	
+	for (let indexRows = 0; indexRows < numberOfRows; indexRows++) {
+		let row = [];
+		for (let indexColumn = 0;  indexColumn < numberOfColumns; indexColumn++) {
+			row.push(null);
+		}
+		board.push(row);
+	}
+	
+	let numberOfBombsPlaced = 0;
+	
+	while (numberOfBombsPlaced < numberOfBombs) {
+		
+		let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+		let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+		board[randomRowIndex][randomColumnIndex] = 'B';
+		numberOfBombsPlaced++
+		// After Control flow fix potential bomb in the same place
+	}
+		
+	return board;
+};
+
+// Rysowanie poprawnej planszy do gry 
+const printBoard = board => {
+	
+	console.log(board.map(row => row.join(' | ')).join('\n'));
+	
+};
+
+
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+
+console.log('Player Board: '); 
+console.log(printBoard(playerBoard));
+
+console.log('Bomb Board: '); 
+console.log(printBoard(bombBoard));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
