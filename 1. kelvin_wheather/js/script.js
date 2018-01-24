bc1c479220f1285362ad24388a97fcd14cd479c5
Damian Watroba <watek-177@o2.pl>
2018-01-24 22:37:31 +0100

    zad 1 podpiecie do html

--- 1. kelvin_wheather/js/script.js
+++ 1. kelvin_wheather/js/script.js
@@ -1,8 +1,28 @@
-// Kelvin value
-const kelvin = prompt('What is the Kelvin temperature today?');
-// Celsius value - 273
-const celsius = kelvin - 273;
 
-const fahrenheit = celsius*(9/5) + 32;
-let resultFah = Math.floor(fahrenheit);
-console.log(`The temperature is ${resultFah} degrees fahrenheit.`);
+//Function getTemp 
+function getTemp() {
+    // temp get value from input
+    let temp = document.getElementById('temp').value;
+    
+    // div where we put result
+    let resultFahrenheit = document.getElementById('result-fahrenheit');
+    let resultCelsius = document.getElementById('result-celsius');
+    
+    // calculations
+    const celsius = temp - 273;
+    const fahrenheit = celsius*(9/5) + 32;
+    let resultFah = Math.floor(fahrenheit);
+    
+    // We put result in html
+    resultCelsius.innerHTML = `The temperature is ${celsius} degrees celsius.`;
+    resultFahrenheit.innerHTML = `The temperature is ${resultFah} degrees fahrenheit.`;
+};
+
+// 
+const submitBtn = document.getElementById('get-value');
+submitBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    
+    getTemp();
+});
+
