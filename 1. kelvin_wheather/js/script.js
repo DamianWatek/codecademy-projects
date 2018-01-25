27b0132c70c85c731b9ca24821ffa3a43f91150b
Damian Watroba <watek-177@o2.pl>
2018-01-25 19:36:28 +0100

    poprawka

--- 1. kelvin_wheather/js/script.js
+++ 1. kelvin_wheather/js/script.js
@@ -4,14 +4,14 @@ function getTemp() {
     // temp get value from input
     let temp = document.getElementById('temp').value;
     
-    // div where we put result
-    let resultFahrenheit = document.getElementById('result-fahrenheit');
-    let resultCelsius = document.getElementById('result-celsius');
-    
     // calculations
     const celsius = temp - 273;
     const fahrenheit = celsius*(9/5) + 32;
     let resultFah = Math.floor(fahrenheit);
+    
+    // div where we put result
+    let resultFahrenheit = document.getElementById('result-fahrenheit');
+    let resultCelsius = document.getElementById('result-celsius');
     
     // We put result in html
     resultCelsius.innerHTML = `The temperature is ${celsius} degrees celsius.`;
