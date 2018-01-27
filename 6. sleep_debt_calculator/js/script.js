4303cd084924ff18cf76eded6dceb1884d61735b
Damian Watroba <watek-177@o2.pl>
2018-01-27 18:04:28 +0100

    zad 6

--- 6. sleep_debt_calculator/js/script.js
+++ 6. sleep_debt_calculator/js/script.js
@@ -1,49 +1,61 @@
+
 const getSleepHours = day => {
-  if (day === 'Monday') {
-    return 8;
-  } else if (day === 'Tuesday') {
-    return 8;
-  } else if (day === 'Wednesday') {
-    return 8;
-  } else if (day === 'Thursday') {
-  	return 8;
-  } else if (day === 'Friday') {
-    return 10;
-  } else if (day === 'Saturday') {
-    return 10;
-  } else if (day === 'Sunday') {
-    return 8;
-  } 
+    const monVal = Number(document.getElementById('sleepVal-1').value);
+    const tueVal = Number(document.getElementById('sleepVal-2').value);
+    const wedVal = Number(document.getElementById('sleepVal-3').value);
+    const thuVal = Number(document.getElementById('sleepVal-4').value);
+    const friVal = Number(document.getElementById('sleepVal-5').value);
+    const satVal = Number(document.getElementById('sleepVal-6').value);
+    const sunVal = Number(document.getElementById('sleepVal-7').value);
+    
+    if (day === 'Monday') {
+        return monVal;
+      } else if (day === 'Tuesday') {
+        return tueVal;
+      } else if (day === 'Wednesday') {
+        return wedVal;
+      } else if (day === 'Thursday') {
+        return thuVal;
+      } else if (day === 'Friday') {
+        return friVal;
+      } else if (day === 'Saturday') {
+        return satVal;
+      } else if (day === 'Sunday') {
+        return sunVal;
+      } 
 };
 
-const getActualSleepHours = () => getSleepHours('Monday') + getSleepHours('Tuesday') + getSleepHours('Wednesday') + getSleepHours('Thursday') + getSleepHours('Friday') + getSleepHours('Saturday') + getSleepHours('Sunday');
+
+const getActualSleepHours = () => getSleepHours('Monday') + getSleepHours('Tuesday') + getSleepHours('Wednesday') + getSleepHours('Thursday') + getSleepHours('Friday') + getSleepHours('Saturday') + getSleepHours('Sunday'); 
+
 
 const getIdealSleepHours = () => {
-  let idealHours = 10;
+  const idealHours = Number(document.getElementById('idealVal').value);
   return idealHours * 7;
 };
 
 const calculateSleepDebt = () => {
   const actualSleepHours = getActualSleepHours();
   const idealSleepHours = getIdealSleepHours();
-  
-  if (actualSleepHours ===  idealSleepHours) {
-    console.log('yea? enough');
+  const result = document.getElementById('result');
+    
+  if (actualSleepHours === idealSleepHours) {
+   result.innerHTML = 'I think you should be rested';
+  } else if (actualSleepHours > idealSleepHours) {
+    result.innerHTML = 'You need more sleep';
+  } else if (actualSleepHours <  idealSleepHours) {
+    result.innerHTML = 'Wake up! You sleep too long! :D';
   } 
-
-  if (actualSleepHours > idealSleepHours) {
-    console.log('Ready to work!');
-  } 
-
-  if (actualSleepHours <  idealSleepHours) {
-    console.log('Get some rest!');
-  }
-
 };
   
 
+const calcBtn = document.getElementById('get-value');
+calcBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    
+    calculateSleepDebt();
+});
 
-calculateSleepDebt();
 
 
 
