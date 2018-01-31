4e077554ffc53a8e24dc2e6c8b91aeeef841c976
Damian Watroba <watek-177@o2.pl>
2018-01-31 22:11:09 +0100

    part 3!

--- ravenous_react_app/src/App.js
+++ ravenous_react_app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+
 import BusinessList from './components/BusinessList/BusinessList'
 import SearchBar from './components/SearchBar/SearchBar'
 
@@ -25,15 +26,18 @@ const businesses = [
 ];
 
 class App extends React.Component {
-  render() {
-    return (
-        <div className="App">
-            <h1>ravenous</h1>
-            <SearchBar />
-            <BusinessList  businesses={businesses} />
-        </div>
-    );
-  }
+    searchYelp(term, location, sortBy) {
+        console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+    };
+    render() {
+        return (
+            <div className="App">
+                <h1>ravenous</h1>
+                <SearchBar searchYelp={this.searchYelp} />
+                <BusinessList  businesses={businesses} />
+            </div>
+        );
+    }
 }
 
 export default App;
